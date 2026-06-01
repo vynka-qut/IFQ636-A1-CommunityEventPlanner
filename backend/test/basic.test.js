54c20afaaca36cc8def479c0d8a9d6b41e4e574c
Vynka Smith <YOUR_GITHUB_EMAIL>
2026-06-01 13:58:43 +0000

    Add basic backend test

---
+++ backend/test/basic.test.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai');
+
+describe('Community Event Planner basic test', function () {
+  it('should confirm the test environment is working', function () {
+    expect(true).to.equal(true);
+  });
+});
